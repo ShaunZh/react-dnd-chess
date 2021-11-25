53138b94efc0989e8935d73bd31b457a5a92be55
zhangjie <hexon804@163.com>
2021-11-25 14:12:13 +0800

    add: 使用react-dnd的拖动预览图片功能

--- src/pages/ReactDndChess/Knight.tsx
+++ src/pages/ReactDndChess/Knight.tsx
@@ -3,27 +3,31 @@
  * @Author: Zhang jie
  * @Date: 2021-11-24 17:31:34
  * @LastEditors: Zhang jie
- * @LastEditTime: 2021-11-25 11:26:42
+ * @LastEditTime: 2021-11-25 14:09:59
  */
 
 import React from 'react';
 import { ItemTypes } from './Constants';
-import { useDrag } from 'react-dnd';
+import { useDrag, DragPreviewImage } from 'react-dnd';
+import { knightImage } from './knightImage';
 
 export default function Knight(): React.ReactElement {
-  const [{ isDragging }, drag] = useDrag(() => ({
+  const [{ isDragging }, drag, preview] = useDrag(() => ({
     type: ItemTypes.KNIGHT,
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging()
     })
   }));
 
-  return <span
-    ref={drag}
-    style={{
-      opacity: isDragging ? 0.5 : 1,
-      fontSize: 64,
-      fontWeight: 'bold',
-      cursor: 'move',
-    }}>♘</span>;
+  return <>
+    <DragPreviewImage connect={preview} src={knightImage}></DragPreviewImage>
+    <span
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 64,
+        fontWeight: 'bold',
+        cursor: 'move',
+      }}>♘</span>
+  </>;
 }
